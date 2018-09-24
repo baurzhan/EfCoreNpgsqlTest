26b359b5b847bc48aff6d95d91dfb6cc7deb7070
Baurzhan Taishinov <baurzhan@gmail.com>
2018-09-24 12:04:46 +0600

    Add npgsql logging configuration

---
+++ MyNpgsqlLogger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Npgsql.Logging;
+using Xunit.Abstractions;
+
+namespace Optiro.Data.Tests
+{
+    public class MyNpgsqlLogger : NpgsqlLogger
+    {
+        private readonly ITestOutputHelper _outputHelper;
+        public MyNpgsqlLogger(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        public override bool IsEnabled(NpgsqlLogLevel level)
+        {
+            return true;
+        }
+
+        public override void Log(NpgsqlLogLevel level, int connectorId, string msg, Exception exception = null)
+        {
+            _outputHelper.WriteLine(msg);
+        }
+    }
+}
---
+++ NpgsqlLogginProvider.cs
@@ -0,0 +1,20 @@
+﻿using Npgsql.Logging;
+using Xunit.Abstractions;
+
+namespace Optiro.Data.Tests
+{
+    public class NpgsqlLogginProvider : INpgsqlLoggingProvider
+    {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public NpgsqlLogginProvider(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        public NpgsqlLogger CreateLogger(string name)
+        {
+            return new MyNpgsqlLogger(_outputHelper);
+        }
+    }
+}
